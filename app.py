0e97abd3a3470d15e67f5981b4a6bf24ec5c8d51
PeterzGitAcc <92164329+PeterzGitAcc@users.noreply.github.com>
2025-02-14 06:56:44 +0000

    gemini chatbot api app example

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import request, render_template
+import google.generativeai as genai
+import os
+
 
 """
 https://flask.palletsprojects.com/en/stable/quickstart/
@@ -12,7 +15,9 @@ pip install -r requirements.txt
 
 Would help to have a venv too
 """
-
+os.getenv("makesuite")
+genai.configure(api_key="makersuite")
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 app = Flask(__name__)
 
@@ -20,6 +25,17 @@ app = Flask(__name__)
 #landing page is always index
 def index():
     return render_template("index.html")
+
+@app.route("/makersuite",methods=["GET","POST"])
+#landing page is always index
+def makersuite():
+    return render_template("makersuite.html")
+
+@app.route("/gemini",methods=["GET","POST"])
+def gemini():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("gemini.html",r= r.candidates[0].content.parts[0].text))
 
 if __name__ == "__main__":
     app.run()
